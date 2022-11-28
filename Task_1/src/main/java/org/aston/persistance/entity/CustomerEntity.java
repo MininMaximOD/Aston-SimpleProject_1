a55a8af486e9a3b5787ea09b57e00d1e0d82805e
MininMaxim <mininmaxim87@gmail.com>
2022-11-28 13:48:42 +0300

    Настройка связей между сущностями
    Настройка связей One-to-One, One-to-Many, Many-to-Many
    Реализация методов добавления и удаление сущностей согласно их связей.

--- Task_1/src/main/java/org/aston/persistance/entity/CustomerEntity.java
+++ Task_1/src/main/java/org/aston/persistance/entity/CustomerEntity.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -17,6 +18,18 @@ import java.util.List;
 @Table(name = "customer")
 public class CustomerEntity extends AbstractHumanEntity{
 
-    @OneToMany(mappedBy = "project")
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.PERSIST)
     private List<ProjectEntity> projects = new ArrayList<>();
+
+    public void addProject(ProjectEntity project) {
+        if(project == null) return;
+        if(projects == null) projects = new ArrayList<>();
+        projects.add(project);
+    }
+
+    public void removeProject(ProjectEntity project){
+        if(project == null && projects == null) return;
+        if(projects.contains(project)) projects.remove(project);
+        project.removeCustomer();
+    }
 }
--- Task_1/src/main/java/org/aston/persistance/entity/ProjectEntity.java
+++ Task_1/src/main/java/org/aston/persistance/entity/ProjectEntity.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Setter
 @Getter
@@ -20,12 +22,45 @@ public class ProjectEntity extends AbstractModifyEntity{
     @Column(name = "deadline")
     private Instant dateline;
 
-    @OneToOne(mappedBy = "project_full_info")
+    @OneToOne(mappedBy = "project_full_info", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     private ProjectInfo projectInfo;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "customer_id")
     private CustomerEntity customer;
+
+    @ManyToMany(mappedBy = "projects", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private List<EmployeeEntity> employees;
+
+    public void setProjectInfo(ProjectInfo projectInfo){
+        if(projectInfo == null){
+            this.projectInfo = null;
+        }
+        projectInfo.setProject(this);
+        this.projectInfo = projectInfo;
+    }
+
+    public void setCustomer(CustomerEntity customer){
+        this.customer =customer;
+    }
+
+    public void removeCustomer(){
+        customer = null;
+    }
+
+    public void addEmployee(EmployeeEntity employee){
+        if(employee == null) return;
+        if(employees == null) employees = new ArrayList<>();
+        employees.add(employee);
+        employee.addProject(this);
+    }
+
+    public void removeEmployee(EmployeeEntity employee){
+        if(employee == null) return;
+        if(employees.contains(employee)) employees.remove(employee);
+        employee.removeProject(this);
+    }
+
 
 
 
