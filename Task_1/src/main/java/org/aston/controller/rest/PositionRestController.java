4a58f0b3fc3ee5f2a784b17b77143c667d298c32
MininMaxim <mininmaxim87@gmail.com>
2022-11-30 18:42:45 +0300

    Добавил описание методов для Swaggera

--- Task_1/src/main/java/org/aston/controller/rest/PositionRestController.java
+++ Task_1/src/main/java/org/aston/controller/rest/PositionRestController.java
@@ -1,5 +1,7 @@
 package org.aston.controller.rest;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.aston.persistance.entity.PositionEntity;
 import org.aston.service.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,30 +13,35 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping("/positions")
+@Api(tags = {"Должности"})
 public class PositionRestController {
 
     @Autowired
     private Admin admin;
 
     @GetMapping
+    @ApiOperation(value = "получить список всех должностей")
     public Collection<PositionEntity> homePositionPage(Model model){
         Collection<PositionEntity> allPositions = admin.findAllPositions();
         model.addAllAttributes(allPositions);
         return allPositions;}
 
     @PostMapping("/save")
+    @ApiOperation(value = "добавить новую должность")
     public RedirectView save(@ModelAttribute("positionSave") PositionEntity position) {
         admin.savePosition(position);
         return new RedirectView("positions");
     }
 
     @PostMapping("/delete")
+    @ApiOperation(value = "удалить должность")
     public RedirectView delete(@ModelAttribute("positionDelete") PositionEntity position){
         admin.deletePosition(position);
         return new RedirectView("positions");
     }
 
     @PostMapping("/update")
+    @ApiOperation(value = "обновить информацию должности")
     public RedirectView update(@ModelAttribute("positionUpdate") PositionEntity position){
         admin.updatePosition(position);
         return new RedirectView("positions");
